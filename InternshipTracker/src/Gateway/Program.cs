1f5e73428462c9bb59f86ded8ae3df3be86976aa
KravchenkoKseniia <kkravchenko@kse.org.ua>
2026-03-26 00:09:08 +0200

    Added Gateway

--- InternshipTracker/src/Gateway/Program.cs
+++ InternshipTracker/src/Gateway/Program.cs
@@ -1,6 +1,21 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.Use(async (context, next) =>
+{
+    const string correlationIdHeader = "X-Correlation-ID";
+    if (!context.Request.Headers.ContainsKey(correlationIdHeader)) {
+        context.Request.Headers[correlationIdHeader] = Guid.NewGuid().ToString();
+    }
+    
+    var correlationId = context.Request.Headers[correlationIdHeader].ToString();
+    context.Response.Headers[correlationIdHeader] = correlationId;
+
+    await next();
+});
+
+app.MapReverseProxy();
+app.MapGet("/health", () => Results.Ok(new {status = "Healthy"}));
 
 app.Run();
